dd5ee929350608124446735833ea09040282c0c4
Zawad Mutawassit <zawadmann@gmail.com>
2024-10-23 02:50:46 -0230

    Updates

---
+++ precache-manifest.c602cbcd8b46f4d0033d2eb4c577ba56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfbc15bd9a96b88edca0506f0a1df29",
+    "url": "/thrift-mall/index.html"
+  },
+  {
+    "revision": "4c332b89694d5fc9c9ec",
+    "url": "/thrift-mall/static/css/main.5f15ca6d.chunk.css"
+  },
+  {
+    "revision": "0078ef4eb52ba0e1187a",
+    "url": "/thrift-mall/static/js/2.0ea76f5c.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/thrift-mall/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "4c332b89694d5fc9c9ec",
+    "url": "/thrift-mall/static/js/main.81f90dd8.chunk.js"
+  },
+  {
+    "revision": "16e45fcd6a65a1e5ba8b",
+    "url": "/thrift-mall/static/js/runtime~main.2beb3945.js"
+  }
+]);
